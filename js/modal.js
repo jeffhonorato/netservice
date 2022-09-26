8e11c5c7c1499e998ac495d33a26bac815f286f9
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-09-26 15:44:09 -0300

    Ajustes

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,15 @@
-const btnAtendimento = document.querySelector(".js-atendimento");
-const modal = document.querySelector(".js-modal-box");
-const btnFechar = document.querySelector(".js__modal--btnFechar");
+const modalBox = () => {
+    const btnAtendimento = document.querySelector(".js-atendimento");
+    const modal = document.querySelector(".js-modal-box");
+    const btnFechar = document.querySelector(".js__modal--btnFechar");
+    
+    btnAtendimento.addEventListener("click", () => {
+        modal.classList.add("ativo");
+    });
+    
+    btnFechar.addEventListener("click", () => {
+        modal.classList.remove("ativo");
+    });
+};
 
-btnAtendimento.addEventListener("click", () => {
-    modal.classList.add("ativo");
-});
-
-btnFechar.addEventListener("click", () => {
-    modal.classList.remove("ativo");
-});
+modalBox();
