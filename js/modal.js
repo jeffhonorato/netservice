d49358c1b5eae827583aab8e0a324d841d0922e1
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-09-27 10:08:15 -0300

    Ajustes

--- js/modal.js
+++ js/modal.js
@@ -2,9 +2,11 @@ const modalBox = () => {
     const btnAtendimento = document.querySelector(".js-atendimento");
     const modal = document.querySelector(".js-modal-box");
     const btnFechar = document.querySelector(".js__modal--btnFechar");
+    const bodyScroll = document.querySelector(".js-scrooll");
     
     btnAtendimento.addEventListener("click", () => {
         modal.classList.add("ativo");
+        
     });
     
     btnFechar.addEventListener("click", () => {
@@ -12,4 +14,21 @@ const modalBox = () => {
     });
 };
 
+const modalScroolDesativado = () => {
+    const boxModal = document.querySelector(".js-modal-box");
+    const atendimentoBtn = document.querySelector(".js-atendimento");
+    const bodyScroll = document.querySelector(".js-scrooll");
+    const buttonFechar = document.querySelector(".js__modal--btnFechar");
+    
+    atendimentoBtn.addEventListener("click", () => {
+        bodyScroll.classList.add("ativo");
+    });
+
+    buttonFechar.addEventListener("click", () => {
+        bodyScroll.classList.remove("ativo");
+    });
+}
+
+
 modalBox();
+modalScroolDesativado()
